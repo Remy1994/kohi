97d93734ac6f4df8ece316ada9d679f8f44650d1
Remy1994 <15700079681@139.com>
2025-04-09 14:19:06 +0800

    kohi day 6

---
+++ engine/src/entry.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "game_types.h"
+#include "core/logger.h"
+
+extern b8 create_game(game* out_game);
+
+int main(void) {
+    game game_inst;
+    if (!create_game(&game_inst)) {
+        KFATAL("Failed to create game!");
+        return -1;
+    }
+
+    if (!game_inst.render || !game_inst.initialize || !game_inst.on_resize || !game_inst.update) {
+        KFATAL("The game's function pointers must be assigned!");
+        return -2;
+    }
+
+    if (!application_create(&game_inst)) {
+        KINFO("Application create failed!");
+        return 1;
+    }
+
+    if (!application_run()) {
+        KINFO("Application did not shutdown gracefully.");
+        return 2;
+    }
+    
+    return 0;
+}
---
+++ testbed/src/entry.c
@@ -0,0 +1,20 @@
+#include <entry.h>
+#include "game.h"
+#include <platform/platform.h>
+
+b8 create_game(game* game_inst) {
+    game_inst->app_config.name = "Kohi Engine Testbed";
+    game_inst->app_config.start_pos_x = 100;
+    game_inst->app_config.start_pos_y = 100;
+    game_inst->app_config.start_width = 1280;
+    game_inst->app_config.start_height = 720;
+
+    game_inst->initialize = game_initialize;
+    game_inst->on_resize = game_on_resize;
+    game_inst->render = game_render;
+    game_inst->update = game_update;
+
+    game_inst->state = platform_allocate(sizeof(game_state), FALSE);
+
+    return TRUE;
+}
